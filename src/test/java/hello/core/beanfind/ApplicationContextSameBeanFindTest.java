99dfe21c0d656e947a3a48f09b7b818595e43ac0
jisu <jiiisu0227@gmail.com>
2021-08-06 17:48:22 +0900

    TEST: SameBeanConfig class 생성 및 동일한 타입의 스프링 빈 등록 추가

---
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,33 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextSameBeanFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+
+    @Configuration
+    static class SameBeanConfig {
+        @Bean
+        public MemberRepository memberRepository1 () {
+            return new MemoryMemberRepository();
+        }
+        @Bean
+        public MemberRepository memberRepository2 () {
+            return new MemoryMemberRepository();
+        }
+    }
+
+}
