cdadf60b5cf99938a8bb5051cc2102f75154ccf4
jisu <jiiisu0227@gmail.com>
2021-08-06 18:00:56 +0900

    TEST: 동일한 타입의 빈이 여러개 등록되어 있을 경우 이름을 지정하여 조회하는 방법 테스트 추가

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -26,12 +26,20 @@ public class ApplicationContextSameBeanFindTest {
     }
 
 
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다")
+    void findBeanByName() {
+        MemberRepository memberRepository = ac.getBean("memberRepository1",MemberRepository.class);
+        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
+    }
+
     @Configuration
     static class SameBeanConfig {
         @Bean
         public MemberRepository memberRepository1 () {
             return new MemoryMemberRepository();
         }
+
         @Bean
         public MemberRepository memberRepository2 () {
             return new MemoryMemberRepository();
